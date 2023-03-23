1510e200a34e73885048d3df988e32d3c0b193ed
Carlos Cataluña Ronda <carloscataronda@gmail.com>
2023-03-23 16:21:36 +0100

    a ver

--- src/controllers/rellenar_fechas/endpoint.js
+++ src/controllers/rellenar_fechas/endpoint.js
@@ -7,9 +7,13 @@
 const Bloques_vias = require("../../models/Bloques_vias");
 
 const endpoint = async (req, res) => {
-  const bloques = await Bloques_vias.find(query);
-  return res.status(200).json({
-    vias: bloques,
+  await Bloques_vias.find({}).exec((error, bloques) => {
+    // ejecuto.find sobre una CLASE Bloques_vias, bloques es un array con objetos
+
+    return res.status(200).json({
+      status: "Éxito, aquí están los itinerarios de la pared de 15",
+      vias: bloques,
+    });
   });
 };
 
