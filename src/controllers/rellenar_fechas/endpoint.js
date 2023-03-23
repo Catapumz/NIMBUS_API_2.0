fa6284e8ee6f9b366e34ebb800ef90bbfc1de3c2
Carlos Cataluña Ronda <carloscataronda@gmail.com>
2023-03-23 17:48:00 +0100

    dev-option rellenar fechas

--- src/controllers/rellenar_fechas/endpoint.js
+++ src/controllers/rellenar_fechas/endpoint.js
@@ -1,25 +1,9 @@
-// await Bloques_Vias.updateMany(
-//     {},
-//      { $set: { dateCreation: ISODate("2023-03-23") } }
-//   )
-//  const fecha = "2023-" + i1 + "-" + j1;
-
 const Bloques_vias = require("../../models/Bloques_vias");
 const { ala } = require("./yearfunction");
 
 const endpoint = async (req, res) => {
   const bloques = await Bloques_vias.find({});
   fechas = ala(bloques.length);
-
-  // await Bloques_vias.updateMany(
-  //   {},
-  //   { $set: { dateCreation: ISODate(fechas) } }
-  // );
-
-  // const bloques1 = await Bloques_vias.find({});
-  // return res.status(200).json({
-  //   bloques1,
-  // });
 
   await Promise.all(
     bloques.map(async (bloque, index) => {
